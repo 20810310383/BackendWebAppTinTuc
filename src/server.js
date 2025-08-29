5ea71a5c3caf64a516d5560191635864dc67a335
Do Khac Tu <dokhactu1822@gmail.com>
2025-08-30 00:22:43 +0700

    upp

--- src/server.js
+++ src/server.js
@@ -37,6 +37,7 @@ connectDB();
 // Cài đặt CORS
 const allowedOrigins = [
     'http://localhost:3070',     
+    'http://localhost:307',     
     'https://dantri24h.com',     
 ];
 const cookieParser = require('cookie-parser');
